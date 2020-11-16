73ae6e4476f4bdd1d8d92b412f155253450403f0
emmacps <emmacps001@gmail.com>
2020-11-16 19:16:37 +0000

    working with arrays and loop

---
+++ loop.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOOP STATEMENT</title>
+</head>
+<body>
+
+<?php 
+
+// $i = 1;
+
+// while($i <= 3){
+
+// echo "The number is " . $i . "<br>";
+
+// $i++;
+
+$i = 1;
+do{
+	
+	echo "The number is " . $i . "<br>";
+	$i++;
+
+}
+
+while($i <= 3);
+	
+
+
+ ?>
+
+</body>
+</html>
