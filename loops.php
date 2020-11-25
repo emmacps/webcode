48db450e437015a76b2795a186880a73b4988a6f
Emmanuel Ntiamoah <emmacps001@gmail.com>
2020-11-25 19:18:35 +0000

    Add files via upload

---
+++ loops.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loops</title>
+	<?php include 'functions.php'; ?>
+</head>
+<body>
+
+	<?php 
+
+	//while loop
+
+	// $i = 1;
+
+	// while($i <=5 ){
+
+	// 	echo "The number is " . $i . "<br>";
+	// 	$i++;
+	// }
+
+	//do while
+	// $i = 1;
+
+	// do{
+		
+	// 	echo "The number is " . $i . "<br>";
+
+	// 	$i++;
+	// }
+
+	// while($i < 5 );
+
+	//for loop
+
+	// for($i=1; $i<=3; $i++){
+
+	// 	echo "The number is " . $i . "<br>";
+	// }
+
+	//foreach
+
+	$colors = array("Red", "Green", "Blue");
+
+	foreach($colors as $value){
+		echo $value . "<br>";
+	}
+
+   echo "<br>";
+	$superhero = array(
+		"name" => "Peter Parker",
+		"email" => "peter@gmail.com",
+		"age" => 18
+	);
+
+	foreach ($superhero as $key => $value) {
+		echo $key . " : " . $value;
+	}
+
+	 ?>
+
+	 <?php whatistoday(); ?>
+
+</body>
+</html>
